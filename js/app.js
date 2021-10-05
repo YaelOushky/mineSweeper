25dfa47ceed7fd4efc9f8b990498235b0f452fdb
yael oushky <91841517+YaelOushky@users.noreply.github.com>
2021-10-05 19:12:16 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,201 +3,213 @@
 const MINE = '💣'
 const FLAG = '🚩'
 const EMPTY = ' '
+
 var gBoard
 var gGameInterval
 var gLife
-var gNeighborsMainCount
+var gFirstClick
 
-// var gBoard = {  //The model
-//     minesAroundCount: 0,   // מוקשים מסביב לתא
-//     isShown: false,   //האם לחצו עליו
-//     isMine: false,  // האם מוקש
-//     isMarked: false  // האם סומן בדגל
-// }
 var gLevel = {
-    size: 4,
-    mines: 2
+    SIZE: 4,
+    MINES: 2
 }
+
 var gGame = {
-    isOn: false,  //boolean, when true we let the user play
-    shownCount: 0,  // how many cells are shown
-    markedCount: 0,  // how many cells are marked (with a flag)
-    secsPassed: 0  // how many seconds passed
+    isOn: false,
+    shownCount: 0,
+    markedCount: 0,
+    secsPassed: 0
 }
+
+
+var gSoundStartGame = new Audio('sound/start-game.wav')
+var gSoundMine = new Audio('sound/mine.mp3')
+var gSoundGameOver = new Audio('sound/game-over.wav')
+var gSoundWin = new Audio('sound/win.wav')
 
 function init() {
-    // Todo play the game 
+    // play the game 
     gLife = 3
-    gNeighborsMainCount = null
     gGame.isOn = true
-    gGame.shownCount = 0;
-    gGame.markedCount = 0;
-    if (gGame.isOn) setTimer()
-
-    clearInterval(gGameInterval)
-    document.querySelector('.emoji').innerText = '😄'
-    gBoard = createBoard(gLevel.size)
+    gGame.isOn = true
+    gGame.shownCount = 0
+    gGame.markedCount = 0
+    gFirstClick = false
+    gBoard = createBoard(gLevel.SIZE)
     renderBoard(gBoard)
+    clearInterval(gGameInterval)
+    createMine(gBoard, gLevel.MINES)
+
+    document.querySelector('.emoji').innerText = '😄'
+    document.querySelector('.time').innerText = '🕔 00:00'
 }
 
 
 function setLevel(size, mines) {
     //  Support 3 levels of the game onclick level
-    gLevel['size'] = size
-    gLevel['mines'] = mines
+    gLevel['SIZE'] = size
+    gLevel['MINES'] = mines
     var elMines = document.querySelector('.bomes')
     elMines.innerText = mines
+    restartGame()
+}
+
+function restartGame() {
+    gLife = 3
     init()
 }
 
-
 function endGame() {
-    console.log('game over');
-    //loos
-    document.querySelector('.emoji').innerText = '😭'
-    //victory
-    clearInterval(gGameInterval)
-    // document.querySelector('.emoji').innerText = '😎'
+    if (gLife === 0) {
+        gSoundGameOver.play()
+        showAll(gBoard)
+        gGame.isOn = false
+        console.log('game over');
+        document.querySelector('.emoji').innerText = '😭'
+
+    }
+    if (!gGame.isOn) clearInterval(gGameInterval)
+    // createMine(gBoard, gLevel.MINES)
 }
 
 function checkGameOver() {
-    if (gLife === 0) endGame()
-    // Todo
+    if (gGame.shownCount === gLevel.SIZE * gLevel.SIZE) {
+
+        document.querySelector('.emoji').innerText = '😎' // win
+        restartGame()
+    }
     //  LOSE: when clicking a mine, all mines should be revealed
     //  WIN: all the mines are flagged, and all the other cells are shown
 }
 
-function cellMarked(elCell) {
-
-    // Todo Called on right click to mark a cell (suspected to be a mine)
-    //Search the web (and  implement) how to hide the 
-    //context menu on right click
-
-    // if (gBoard[i][j].isMarked ) 
-}
 
 function cellClicked(i, j, elCell, event) {
-    gGame.shownCount++
+    console.log(elCell);
+    if (!gGame.isOn) return
 
-    if (gGame.shownCount === 1) gGame.isOn === true
-
-    gBoard[i][j].isShown = true
-
-    var neighbors = countNeighbors(i, j, gBoard)
-
-    if (gBoard[i][j].isMarked) cellMarked(elCell)
-
-    if (gBoard[i][j].isMine) {
-        var elLife = document.querySelector('.life span')
-        gLife--
-        elLife.innerText = gLife
-        endGame()
+    if (!gFirstClick) {
+        gFirstClick = true
+        gSoundStartGame.play()
+        setTimer()
+        renderBoard()
         return
     }
 
+    gGame.shownCount++
+    gBoard[i][j].isShown = true
 
-    elCell.classList.add('selected');
+    if (event.button === 0) {
+        if (gBoard[i][j].isMine) {
+            gSoundMine.play()
+            document.querySelector('.emoji').innerText = '😕'
+            gLevel.MINES--
+            gBoard[i][j].isShown = true
+            elCell.innerText = MINE
+            elCell.style.backgroundColor = "red"
+            var elLife = document.querySelector('.life span')
+            gLife--
+            elLife.innerText = gLife
+            endGame()
 
-
-    console.log(`i: ${i}, j: ${j}`);
-    // MODEL
-
-    //DOM
-
-    // Todo Called when a cell (td) is clicked
-
-
-}
-
-function setMinesNegsCount(board) {
-    // Todo Count mines around each cell and set the cell's
-    // minesAroundCount
-    var noMineCells = [];
-    for (var i = 0; i < board.length; i++) {
-        for (var j = 0; j < board[0].length; j++) {
-            var negsCount = countNeighbors(i, j, board);
-            var currCellPos = { i, j, negsCount };
-            var currCell = board[i][j];
-            if (!currCell.isMine) {
-                noMineCells.push(currCellPos);
-
-            }
         }
+        // if (gBoard[i][j].minesAroundCount === 0) {
+        //     openAllNeighbors(i, j, gBoard, event)
+        // }
     }
-    return noMineCells;
-}
-
-
-
-function countNeighbors(cellI, cellJ, mat) {
-    var neighborsCount = 0;
-    for (var i = cellI - 1; i <= cellI + 1; i++) {
-        if (i < 0 || i >= mat.length) continue;
-        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-            if (i === cellI && j === cellJ) continue;
-            if (j < 0 || j >= mat[i].length) continue;
-            mat[i][j].isShown = true
-
-            var elCell = document.querySelector(`[data-i="${i}"][data-j="${j}"]`)
-            elCell.innerText = '';
-            elCell.classList.add('selected') // מוחק שכנים
-
-            if (mat[i][j]) neighborsCount++;
-            if (mat[i][j].isMine) gNeighborsMainCount++;
+    if (event.button === 2) {
+        // event.preventDefault()
+        if (gBoard[i][j].isMarked) {
+            gBoard[i][j].isMarked = false;
+            gGame.markedCount--
+        } else {
+            gBoard[i][j].isMarked = true;
+            gGame.markedCount++
         }
+
     }
-    console.log('neighborsCount', neighborsCount);
-    return neighborsCount;
+    // elCell.classList.add('selected')
+    checkGameOver()
+    renderBoard(gBoard)
 }
 
 function renderBoard(board) {
-    // Todo Render the board as a <table> to the page
-    var strHTML = '';
+    // Render the board as a <table> to the page
+    var strHTML = ''
     for (var i = 0; i < board.length; i++) {
-        strHTML += '<tr>';
+        strHTML += '<tr>'
         for (var j = 0; j < board[0].length; j++) {
             var className = 'cell'
-            // if (board[i][j].isShown) className += 'selected'
-            if (board[i][j].isMine) strHTML += MINE
-            if (board[i][j].isMarked) strHTML += FLAG
+            var cellContent = ''
+            if (board[i][j].isMarked) cellContent = FLAG
+            if (board[i][j].isShown) {
+                if (board[i][j].isMine) cellContent = MINE
+                else cellContent = board[i][j].minesAroundCount
+                if (board[i][j].minesAroundCount === 0) cellContent = '. '
+            }
             strHTML += `<td data-i="${i}" data-j="${j}"
-             title="${i}-${j}" onclick="cellClicked(${i},${j},this,event)"
-              class="${className}" class="hide">`
+            oncontextmenu="cellClicked(${i},${j},this,event)"
+             onclick="cellClicked(${i},${j},this,event)"
+              class="${className}">${cellContent}</td>`
         }
-        strHTML += '</td>'
         strHTML += '</tr>'
     }
 
     var elBoard = document.querySelector('.board')
     elBoard.innerHTML = strHTML
+
+    var Board = document.querySelector('.board') //  rightckick
+    Board.addEventListener('contextmenu', e => {
+        e.preventDefault()
+    })
 }
 
-
-function createBoard(num = 4) {
+function createBoard(length = 4) {
     var board = [];
-    for (var i = 0; i < num; i++) {
-        board.push([]);
-        for (var j = 0; j < num; j++) {
+    for (var i = 0; i < length; i++) {
+        board[i] = [];
+        for (var j = 0; j < length; j++) {
             board[i][j] = {
-                minesAroundCount: gNeighborsMainCount,
+                minesAroundCount: 0,
                 isShown: false,
                 isMine: false,
                 isMarked: false
             }
         }
     }
-    for (var i = 0; i < gLevel.mines; i++) {
-        var randomI = getRandomInt(1, gLevel.size - 1)
-        var randomJ = getRandomInt(1, gLevel.size - 1)
-        
-        if (board[randomI][randomJ].isMine) {
-            i--
-            continue
+    createMine(board, gLevel.MINES) // creat mine in random place
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[0].length; j++) {
+            board[i][j].minesAroundCount = setMinesNegsCount(board, i, j)
         }
-        board[randomI][randomJ].isMine = true;
     }
-    // setMinesNegsCount(board)
     return board
+}
+
+function setMinesNegsCount(board, cellI, cellJ) {
+    var count = 0
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= board.length) continue;
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (i === cellI && j === cellJ) continue
+            if (j < 0 || j >= board[0].length) continue
+            if (board[i][j].isMine) count++
+        }
+    }
+    return count
+}
+
+function createMine(board, num) {
+    var count = 0
+    for (var i = 0; i < num; i++) {
+        var randomI = getRandomInt(1, gLevel.SIZE - 1)
+        var randomJ = getRandomInt(1, gLevel.SIZE - 1)
+
+        if (board[randomI][randomJ].isMine) continue
+
+        board[randomI][randomJ].isMine = true
+        count++
+    }
+    return count
 }
 
 function setTimer() {
@@ -208,5 +220,14 @@ function setTimer() {
         var time = ((currTime - startTime) / 1000).toFixed(1)
         elModal.querySelector('span').innerHTML = '🕔 ' + time
     }, 1)
+}
+
+function showAll(board) {
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board.length; j++) {
+            board[i][j].isShown = true
+        }
+    }
+    renderBoard(board)
 }
 
--- js/util.js
+++ js/util.js
@@ -23,3 +23,9 @@ min = Math.ceil(min);
 max = Math.floor(max);
 return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+// Convert a location object {i, j} to a selector and render a value in that element
+function renderCell(i, j, value) {
+    var elCell = document.querySelector(`.cell${i}-${j}`);
+    elCell.innerHTML = value;
+}
